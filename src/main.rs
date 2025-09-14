cfb987a91767d45e641d1e3fa9855ca42db9f3ab
hikojlu <extrafreevirusmailbox@gmail.com>
2025-09-14 10:08:55 +0300

    slightly better error handling

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,23 @@
 use nnp::Nnp;
 use std::env;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>>{
     let mut args = env::args();
     args.next().unwrap();
     match args.next().unwrap().as_str() {
         "long" | "l" => println!(
             "{}",
-            Nnp::from(args.next().unwrap().parse::<usize>().unwrap())
+            Nnp::from(args.next().unwrap().parse::<usize>()?)
         ),
         "short" | "s" => println!(
             "{}",
-            Nnp::from(args.next().unwrap().parse::<usize>().unwrap()).short()
+            Nnp::from(args.next().unwrap().parse::<usize>()?).short()
         ),
         "int" | "dec" | "i" | "d" => println!(
             "{}",
-            usize::from(Nnp::try_from(args.collect::<Vec<_>>().join(" ").as_str()).unwrap())
+            usize::from(Nnp::try_from(args.collect::<Vec<_>>().join(" ").as_str())?)
         ),
         _ => panic!("???"),
     }
+    Ok(())
 }
